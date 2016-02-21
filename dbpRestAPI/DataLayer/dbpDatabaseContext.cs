ee52cd6002e90d145a026d442acfe662fa7ac833
Denver Engagement Photographer <info@davebrownphotography.com>
2016-02-21 10:54:56 -0700

    Checkingm in publish settinhg and context

--- dbpRestAPI/DataLayer/dbpDatabaseContext.cs
+++ dbpRestAPI/DataLayer/dbpDatabaseContext.cs
@@ -21,7 +21,7 @@ namespace dbpRestAPI.DataLayer
             //C:\Users\Asus\Documents\Visual Studio 2015\Projects\dbpRestAPI\dbpRestAPI\App_Data
             //C:\dbpRestAPI\dbpRestAPI\App_Data
             //this.Database.Connection.ConnectionString = @"DataSource = C:\dbpRestAPI\dbpRestAPI\App_Data\DBPSqlCompact.sdf";
-            this.Database.Connection.ConnectionString = "DataSource = " + HttpContext.Current.Server.MapPath("~/App_Data/DBPSqlCompact.sdf");
+            this.Database.Connection.ConnectionString = "DataSource = " + System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/DBPSqlCompact.sdf");
         }
 
         public static dbpDatabaseContext Create()
